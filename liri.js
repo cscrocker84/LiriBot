ead91f2d259f670299c9faabfefa966045dda656
Charlie Crocker <cscrocker84@gmail.com>
2019-10-03 18:37:54 -0500

    got getMovie in order, starting on getSong

--- liri.js
+++ liri.js
@@ -26,3 +26,78 @@ function getConcert (artist) {
         }
       })
   }
+//   getConcert();
+
+function getMovie (movie) {
+    if (process.argv[3] === undefined) {
+      axios.get("http://www.omdbapi.com/?t=mr+nobody+&y=&plot=short&apikey=trilogy").then(
+  
+      function(response) {
+  
+        if (response.data.Ratings < 1 || response.data.Ratings == undefined) {
+          rtRating = "Sorry, no Rotten Tomatoes rating is given."
+        }
+        else {
+         rottenRating = response.data.Ratings.filter(entry => entry.Source === "Rotten Tomatoes") 
+         rtRating = rottenRating[0].Value
+        }
+  
+        console.log(
+        "Title: " + response.data.Title + '\n' +
+        "Release Year: " + response.data.Year + '\n' +
+        "IMDB Rating: " + response.data.imdbRating + '\n' +
+        "Rotten Tomatoes Rating: " + rtRating  + '\n' +
+        "Country Movie Premiered: " + response.data.Country + '\n' +
+        "Language: " + response.data.Language + '\n' +
+        "Plot: " + response.data.Plot + '\n' +
+        "Actors: " + response.data.Actors + '\n'
+        )
+      })
+    }
+    else {axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy").then(
+  
+        function (response) {
+      
+          // console.log(response.data)
+          var rottenRating
+          var rtRating
+      
+          if (response.data.Ratings < 1 || response.data.Ratings == undefined) {
+            rtRating = "Sorry, no Rotten Tomatoes rating is given."
+          }
+          else {
+           rottenRating = response.data.Ratings.filter(entry => entry.Source === "Rotten Tomatoes") 
+           rtRating = rottenRating[0].Value
+          }
+        
+          console.log(
+            "Title: " + response.data.Title + '\n' +
+            "Release Year: " + response.data.Year + '\n' +
+            "IMDB Rating: " + response.data.imdbRating + '\n' +
+            "Rotten Tomatoes Rating: " + rtRating  + '\n' +
+            "Country Movie Premiered: " + response.data.Country + '\n' +
+            "Language: " + response.data.Language + '\n' +
+            "Plot: " + response.data.Plot + '\n' +
+            "Actors: " + response.data.Actors + '\n'
+            )
+        })
+        }
+        function getSong (song) {
+            if (process.argv[3] === undefined) {
+              spotify.search({ type: 'track', query: 'the sign'} , function(err, data) {
+                if (err) {
+                  return console.log('Error occurred: ' + err);
+                }
+          
+                console.log(
+                  "Artist: " + data.tracks.items[5].artists[0].name + '\n' +
+                  "Song Name: " + data.tracks.items[5].name +  '\n' +
+                  "Album Name: " + data.tracks.items[5].album.name +  '\n' +
+                  "Spotify Preview Link: " + data.tracks.items[5].external_urls.spotify +  '\n' +
+                  "---"
+                  )
+            
+                
+                })
+            }
+    
